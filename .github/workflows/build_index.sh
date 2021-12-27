83527af897a8d8d1ba25db40cf1d0083af337170
Jehiah Czebotar <jehiah@gmail.com>
2021-12-27 16:17:11 -0500

    build_index.sh: create index of local laws

--- .github/workflows/build_index.sh
+++ .github/workflows/build_index.sh
@@ -15,3 +15,7 @@ done
 echo "building people_active.json"
 jq -c -s "map(select(.IsActive) | del(.FirstName,.LastName,.GUID)) | map(.OfficeRecords = ([.OfficeRecords[]? | del(.GUID, .FullName, .PersonID, .LastModified) ]))" people/*.json > build/people_active.json
 
+echo "building local_laws.json"
+# ActionID=58 == City Charter Rule Adopted
+# this is contains the YEAR a local law is from
+jq -c -s "map(select(.Attachments[]?.Name | contains(\"Local Law \")) | del(.RTF,.Summary,.Text,.IntroDate,.BodyID,.BodyName,.Version,.GUID,.TextID,.StatusID,.TypeID,.TypeName,.AgendaDate,.Sponsors)) | map(.Attachments = ([.Attachments[]? | select(.Name | contains(\"Local Law \")) ] )) | map(.History = ([.History[]? | select(.ActionID == 58)] ))"  introduction/????/????.json > build/local_laws.json
