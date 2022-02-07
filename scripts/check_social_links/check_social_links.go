8eb06374fe5e70e0b032e53aef0004710ede1a72
Jehiah Czebotar <jehiah@gmail.com>
2022-02-07 09:00:26 -0500

    people_metadata.json: fix broken links

---
+++ scripts/check_social_links/check_social_links.go
@@ -0,0 +1,57 @@
+// check_social_links verifies that all social links return a HTTP 200 response
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type PersonMetadata struct {
+	ID                           int
+	FullName, Slug               string
+	District                     int
+	Twitter, TwitterPersonal     string
+	Facebook, FacebookPersonal   string
+	Instagram, InstagramPersonal string
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	filename := flag.String("people-metadata", "../../people/appendix/people_metadata.json", "path to people_metadata.json")
+	flag.Parse()
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var people []PersonMetadata
+	d := json.NewDecoder(f)
+	d.DisallowUnknownFields()
+	err = d.Decode(&people)
+	if err != nil {
+		log.Fatal(err)
+	}
+	okCount := 0
+	for _, p := range people {
+		for _, l := range []string{p.Twitter, p.TwitterPersonal, p.Facebook, p.FacebookPersonal, p.Instagram, p.InstagramPersonal} {
+			if l == "" {
+				continue
+			}
+			resp, err := http.DefaultClient.Get(l)
+			code := 0
+			if resp != nil {
+				code = resp.StatusCode
+			}
+			if err != nil || code > 200 {
+				log.Printf("%d %s error getting %q %s StatusCode:%#v", p.ID, p.FullName, l, err, code)
+			} else {
+				okCount++
+			}
+		}
+	}
+	log.Printf("got HTTP 200 for %d social profile links", okCount)
+
+}
---
+++ scripts/go.mod
@@ -0,0 +1,3 @@
+module github.com/jehiah/nyc_legislation/scripts
+
+go 1.17
