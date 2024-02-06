78658d8dd0bf8873cf501aa98932d330c6280e5d
Jehiah Czebotar <jehiah@gmail.com>
2024-02-05 23:05:19 -0500

    people_metadata.json: finish syntax transition

--- scripts/check_social_links/check_social_links.go
+++ scripts/check_social_links/check_social_links.go
@@ -6,20 +6,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/url"
 	"os"
-	"strings"
 )
 
 type PersonMetadata struct {
-	ID                           int
-	FullName, Slug               string
-	District                     int
-	Twitter, TwitterPersonal     string `json:",omitempty"`
-	Facebook, FacebookPersonal   string `json:",omitempty"`
-	Instagram, InstagramPersonal string `json:",omitempty"`
-	Threads                      string `json:",omitempty"`
-	SocialAccounts               []SocialAccount
+	ID             int
+	FullName, Slug string
+	District       int
+	SocialAccounts []SocialAccount
 }
 
 type SocialAccount struct {
@@ -28,82 +22,6 @@ type SocialAccount struct {
 	Official bool   `json:",omitempty"`
 	Personal bool   `json:",omitempty"`
 	Platform string // twitter | instagram | facebook | threads
-}
-
-func (t PersonMetadata) GetSocialAccounts() []SocialAccount {
-	if len(t.SocialAccounts) > 0 {
-		return t.SocialAccounts
-	}
-	accounts := []SocialAccount{
-		{Username: twitterUsername(t.Twitter), Link: t.Twitter, Platform: "twitter"},
-		{Username: twitterUsername(t.TwitterPersonal), Link: t.TwitterPersonal, Platform: "twitter", Personal: true},
-		{Username: facebookUsername(t.Facebook), Link: t.Facebook, Platform: "facebook"},
-		{Username: facebookUsername(t.FacebookPersonal), Link: t.FacebookPersonal, Platform: "facebook", Personal: true},
-		{Username: instagramUsername(t.Instagram), Link: t.Instagram, Platform: "instagram"},
-		{Username: instagramUsername(t.InstagramPersonal), Link: t.InstagramPersonal, Platform: "instagram", Personal: true},
-		{Username: instagramUsername(t.Threads), Link: t.Threads, Platform: "threads"},
-	}
-	if t.Instagram != "" {
-		accounts = append(accounts, SocialAccount{
-			Username: instagramUsername(t.Instagram),
-			Link:     threadsLink(t.Instagram),
-			Platform: "threads",
-		})
-	}
-	if t.InstagramPersonal != "" {
-		accounts = append(accounts, SocialAccount{
-			Username: instagramUsername(t.InstagramPersonal),
-			Link:     threadsLink(t.InstagramPersonal),
-			Platform: "threads",
-			Personal: true,
-		})
-	}
-	var o []SocialAccount
-	for _, a := range accounts {
-		if a.Link != "" {
-			o = append(o, a)
-		}
-	}
-	return o
-}
-
-func threadsLink(s string) string {
-	s = strings.Replace(s, "https://www.instagram.com/", "https://www.threads.net/@", -1)
-	return strings.TrimSuffix(s, "/")
-}
-
-func twitterUsername(s string) string {
-	if s == "" {
-		return ""
-	}
-	u, err := url.Parse(s)
-	if err != nil {
-		return ""
-	}
-	return "@" + strings.TrimPrefix(u.Path, "/")
-}
-func facebookUsername(s string) string {
-	if s == "" {
-		return ""
-	}
-	u, err := url.Parse(s)
-	if err != nil {
-		return ""
-	}
-	if strings.Contains(u.Path, "profile.php") {
-		return "Facebook"
-	}
-	return strings.Trim(u.Path, "/")
-}
-func instagramUsername(s string) string {
-	if s == "" {
-		return ""
-	}
-	u, err := url.Parse(s)
-	if err != nil {
-		return ""
-	}
-	return strings.Trim(u.Path, "/")
 }
 
 func main() {
@@ -127,7 +45,8 @@ func main() {
 	var out []PersonMetadata
 	for _, p := range people {
 		seen := make(map[string]bool)
-		for _, s := range p.GetSocialAccounts() {
+		social := make([]SocialAccount)
+		for _, s := range p.SocialAccounts {
 			if seen[s.Link] {
 				continue
 			}
@@ -136,7 +55,7 @@ func main() {
 			switch s.Platform {
 			case "twitter":
 				skipCount++
-				p.SocialAccounts = append(p.SocialAccounts, s)
+				social = append(social, s)
 				continue
 			case "threads":
 				err = checkThreads(s.Link)
@@ -147,10 +66,11 @@ func main() {
 				log.Printf("⛔️ %d %s error getting %q %s", p.ID, p.FullName, s.Link, err)
 			} else {
 				log.Printf("✅ %d %s %s", p.ID, p.FullName, s.Link)
-				p.SocialAccounts = append(p.SocialAccounts, s)
+				social = append(social, s)
 				okCount++
 			}
 		}
+		p.SocialAccounts = social
 		out = append(out, p)
 	}
 
