5d526ae2980f414576d60b3b27ac6f00c68bd722
Jehiah Czebotar <jehiah@gmail.com>
2024-02-29 23:44:58 -0500

    build_index.sh: fix typo

--- .github/workflows/build_index.sh
+++ .github/workflows/build_index.sh
@@ -84,7 +84,7 @@ else
     jq -c -s "map({File, Name, Title, Summary, StatusName, LastModified:  ([.History[]? | select(.ActionID == 27 or .ActionID == 33 or .ActionID == 32 or .ActionID == 68 or .ActionID == 58)])[-1]?.Date})" introduction/2022/????.json introduction/2023/????.json > build/search_index_2022-2023.json
 fi
 
-for FILE in resbumit/*.json; do
+for FILE in resubmit/*.json; do
     cp $FILE build/resubmit_$(basename $FILE)
 done
 
